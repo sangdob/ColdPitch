083dcc4953b59dec03ce15e1c8a2c5994332b579
mon0mon <mon0mon@naver.com>
2023-05-13 13:43:24 +0900

    no message

--- src/main/java/com/ColdPitch/domain/controller/HelloApiController.java
+++ src/main/java/com/ColdPitch/domain/controller/HelloApiController.java
@@ -14,6 +14,7 @@ import java.util.*;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 @Slf4j
 public class HelloApiController {
     private final HelloService helloService;
